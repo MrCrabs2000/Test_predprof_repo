55ce68143bdb125e65abd946ed4d98e81717d95f
Vernka <vernka@mail.ru>
2026-03-11 14:29:14 +0300

    fix

--- routes/image_map.py
+++ routes/image_map.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template
 from database.classes import Modules, Points
-
+from functions.map_functions import stations_with_radiuses
 
 image_map = Blueprint('image_map', __name__, template_folder='templates')
 @image_map.route('/image/map/<img_url>')
 def image_map_page(img_url):
-    contex = {
-        'img_path':
+    context = {
+        'img_path': stations_with_radiuses(img_url),
     }
+    return render_template('main.html', **context)
--- routs/routs.py
+++
@@ -1,2 +0,0 @@
-def register_all_blueprints(app):
-    pass
