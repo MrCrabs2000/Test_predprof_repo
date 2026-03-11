3ebb7b81ddc69b5af5a62d624eb34d9b00a61856
AG <maximth11@yandex.ru>
2026-03-11 14:07:44 +0300

    start adding img

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import render_template
+from flask import Flask, render_template
+import os
+
+from routs.routs import register_all_blueprints
 from database.db_session import init_database
 
-from configs.configs import app
-from routs.routs import register_all_blueprints
 
+app = Flask(__name__)
+app.secret_key = 'secret_key'
+os.makedirs('db', exist_ok=True)
+init_database()
 
 register_all_blueprints(app)
 
